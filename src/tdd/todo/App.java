baa82a0a0cecb94bb5c063ab67eeb6ad2503bf20
Kacpu <61886846+Kacpu@users.noreply.github.com>
2021-11-22 02:33:48 +0100

    app: added some functionality and next test

--- src/tdd/todo/App.java
+++ src/tdd/todo/App.java
@@ -1,12 +1,23 @@
 package tdd.todo;
 
+import java.io.InputStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class App {
-    public String addTask() {
-        return null;
-    }
 
-    public Integer getInput() {
+    public int getInput(InputStream inputStream) {
+        Scanner scanner = new Scanner(inputStream);
+        int operationNumber;
 
-        return 0;
+        System.out.print(("Podaj numer operacji, którą chcesz wykonać: "));
+
+        try{
+            operationNumber = scanner.nextInt();
+        } catch (InputMismatchException e) {
+            operationNumber = -1;
+        }
+        scanner.close();
+        return operationNumber;
     }
 }
--- src/test/AppTest.java
+++ src/test/AppTest.java
@@ -2,6 +2,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tdd.todo.App;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
@@ -15,8 +18,22 @@ public class AppTest {
 
     @Test
     public void should_returnNegativeNumber_when_userNotGiveOperationNumberAsInteger(){
-        Integer result = app.getInput();
+        String data = "test";
+        InputStream inputStream = new ByteArrayInputStream(data.getBytes());
+
+        int result = app.getInput(inputStream);
 
         assertTrue(result < 0);
+    }
+
+    @Test
+    public void should_returnGivenNumber_when_userGiveOperationNumberAsInteger(){
+        String data = "1";
+        InputStream inputStream = new ByteArrayInputStream(data.getBytes());
+
+        int result = app.getInput(inputStream);
+
+        int expected = 1;
+        assertEquals(expected, result);
     }
 }
