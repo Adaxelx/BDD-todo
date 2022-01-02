927c01a08c1138ea3f5a50996128602789a1f87b
Adrian Mostowski <adaxelx@o2.pl>
2022-01-02 22:27:54 +0100

    add handling of empty string in add message

--- src/test/stories/AddTaskSteps.java
+++ src/test/stories/AddTaskSteps.java
@@ -6,9 +6,6 @@ import org.jbehave.core.steps.Steps;
 import tdd.todo.Task;
 import tdd.todo.TodoList;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -30,80 +27,6 @@ public class AddTaskSteps extends Steps {
         message="";
         newTask = null;
     }
-    @Given("nie ma żadnych zadań")
-    public void emptyToDoList(){
-        list = new TodoList();
-    }
-
-    @When("użytkownik spróbuje dodać zadanie z uzupełnionym opisem test")
-    public void addTaskToList(){
-        newTask = new Task("test");
-        message = list.addTask(newTask);
-    }
-
-    @Then("zadanie zostanie dodane i pojawi się na liście zadan zadanie z opisem test")
-    public void showResultOfAdd(){
-
-        assertEquals("Pomyślnie dodano zadanie", message);
-
-        ArrayList<Task> tasks = list.getTasks();
-
-        assertEquals(1, list.getTaskCount());
-
-        assertEquals(newTask.getDescription(), tasks.get(0).getDescription());
-    }
-
-
-
-    @Given("nie ma zadań do zapisu")
-    public void emptyToDoListToSaveToFile(){
-        list = new TodoList();
-    }
-
-    @When("uzytkownik spróbuje wykonac zapis do pliku")
-    public void tryToSaveEmptyToDoListToFile(){
-        message = list.saveToFile();
-    }
-
-    @Then("zapis nie jest wykonany i wyświetlany jest komunikat o braku zadań")
-    public void showResultOfSaveEmptyToDoListToFile(){
-
-        assertEquals("Nie ma zadań do zapisania!", message);
-
-        ArrayList<Task> tasks = list.getTasks();
-
-        assertEquals(0, list.getTaskCount());
-
-    }
-
-
-    @Given("istnieją jakieś zadania na liście")
-    public void notEmptyToDoList(){
-        list = new TodoList();
-        newTask = new Task("test");
-        message = list.addTask(newTask);
-
-        assertEquals("Pomyślnie dodano zadanie", message);
-    }
-
-    @When("użytkownik spróbuje wykonac zapis do pliku")
-    public void tryToSaveToDoListToFile(){
-        message = list.saveToFile();
-    }
-
-    @Then("zapis został wykonany pomyślnie i została wyświetlona ściezka do pliku")
-    public void showResultOfSaveToDoListToFile(){
-
-        String dir = System.getProperty("user.dir");
-        dir += "\\savedTasksFiles\\TodoList.txt";
-        try {
-            Files.delete(Path.of(dir));
-        } catch (IOException e) {}
-
-        String expected = "Pomyślnie zapisano do pliku o ścieżce: " + dir;
-        assertEquals(expected, message);
-    }
-
 
     @Given("istnieje zadanie z opisem test1")
     public void taskWithDescription(){
@@ -114,7 +37,11 @@ public class AddTaskSteps extends Steps {
 
     @When("uzytkownik sprobuje dodac zadanie z opisem=<description>")
     public void addTaskToList(@Named("description") String description){
-        newTask = new Task(description);
+        if(description==""){
+            newTask = new Task(null);
+        }else{
+            newTask = new Task(description);
+        }
         message = list.addTask(newTask);
     }
 
@@ -131,9 +58,4 @@ public class AddTaskSteps extends Steps {
             assertEquals(newTask.getDescription(), tasks.get(length-1).getDescription());
         }
     }
-
-
-
-
-
 }
--- src/test/stories/AddTaskSteps.story
+++ src/test/stories/AddTaskSteps.story
@@ -1,22 +1,3 @@
-Scenario: Kiedy użytkownik doda zadanie do pustej listy pojawi się ono na liście
-
-Given nie ma żadnych zadań
-When użytkownik spróbuje dodać zadanie z uzupełnionym opisem test
-Then zadanie zostanie dodane i pojawi się na liście zadan zadanie z opisem test
-
-Scenario: Kiedy użytkownik chce zapisać pustą listę do pliku
-
-Given nie ma zadań do zapisu
-When uzytkownik spróbuje wykonac zapis do pliku
-Then zapis nie jest wykonany i wyświetlany jest komunikat o braku zadań
-
-Scenario: Kiedy użytkownik chce zapisać listę zadań do pliku i otrzymuje komunikat o pomyślnym zapisie
-
-Given istnieją jakieś zadania na liście
-When użytkownik spróbuje wykonac zapis do pliku
-Then zapis został wykonany pomyślnie i została wyświetlona ściezka do pliku
-
-
 Scenario: Jako uzytkownik chce dodac zadanie do listy zadan
 
 Given istnieje zadanie z opisem test1
